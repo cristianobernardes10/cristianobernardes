69efe47188745168873d5468c1f075e9ff021ffd
Noturno <Noturno@192.168.7.181>
2017-05-18 19:03:43 -0300

    comit inicial

---
+++ algoritmos/src/exemplos/lacos/ValorDaSoma.java
@@ -0,0 +1,23 @@
+package exemplos.lacos;
+
+public class ValorDaSoma {
+	
+	public static void main(String[] args) {
+		System.out.println("O valor da soma é" + soma);
+		O valor da soma é 120
+
+
+
+
+
+
+		int _valorMensal = 10;
+		int _qtdMeses = 12;
+		double soma = 0;
+		for(int aux = 0; aux < _qtdMeses; aux++){
+			soma += _valorMensal;
+		}
+
+	}
+
+}
---
+++ algoritmos/src/exemplos/lacos/ValorMensal.java
@@ -0,0 +1,19 @@
+package exemplos.lacos;
+
+public class ValorMensal {
+	
+	public static void main(String[] args) {
+		Laços
+
+		int _valorMensal = 10;
+		int _qtdMeses = 12;
+		int aux = 0;
+		double soma = 0;
+		while(aux < _qtdMeses){
+			soma += _valorMensal;
+			aux++;
+		}
+
+	}
+
+}
